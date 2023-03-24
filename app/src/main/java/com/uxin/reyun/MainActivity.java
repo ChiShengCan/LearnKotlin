b385a67543261f3242174cc23d8acc0c2a980374
wangshu <wangshu@uxin.com>
2023-03-24 19:34:31 +0800

    [comments]：基础工程搭建 [jira_id]：wangshu [version]：

---
+++ app/src/main/java/com/uxin/reyun/MainActivity.java
@@ -0,0 +1,16 @@
+package com.uxin.reyun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.reyun.tracking.sdk.Tracking;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
